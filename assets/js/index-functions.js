d90d8f833d6faa87a07d4ac0151606dfd96ced9e
paulbarrett79 <paulbarrett79@gmail.com>
2014-10-08 14:45:25 +0100

    Refactoring index.html javascript to separate file (to be minimised)

---
+++ assets/js/index-functions.js
@@ -0,0 +1,59 @@
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+$('#blog-widget').FeedEk({
+    FeedUrl : 'https://chembiohub.ox.ac.uk/blog/feed.xml',
+  MaxCount : 3,
+    ShowDesc : true,
+    ShowPubDate:true,
+    DescCharacterLimit:300,
+    TitleLinkTarget:'_blank'
+  });
+
+$('#forum-widget').FeedEk({
+    FeedUrl : 'http://chembiohub.ox.ac.uk/askbot/feeds/rss/',
+  MaxCount : 5,
+    ShowDesc : true,
+    ShowPubDate:true,
+    DescCharacterLimit:100,
+    TitleLinkTarget:'_blank'
+  });
+
+$('.ppl-thumb').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8
+});
+
+vpw = $(window).width();
+vph = $(window).height();
+if($('.functional').height() < vph) {
+	$('.functional').height(vph);	
+}
+$(window).keypress(function(e) {
+    if (e.keyCode == 13) {
+        redirect_search();
+    }
+});
+$("#equip-search-btn").click(function(e){
+	e.preventDefault();
+	redirect_search();
+});
+
+function redirect_search(){
+	var v = $("input#equip-search").val();
+	if(v){
+			window.location = "https://www.research-facilities.ox.ac.uk/account/webauth/?next=/search/?q=" + v + "&filter.basedNear.uri=&filter.formalOrganisation.uri=http%3A%2F%2Foxpoints.oucs.ox.ac.uk%2Fid%2F00000000";
+	}
+}
