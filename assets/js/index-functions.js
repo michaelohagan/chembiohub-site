4a971157e6194452faf5bc5681f3fbaa49a23a71
paulbarrett79 <paulbarrett79@gmail.com>
2014-10-15 16:14:09 +0100

    Fixing back button on scroll to FP entry points and contact section

--- assets/js/index-functions.js
+++ assets/js/index-functions.js
@@ -1,18 +1,20 @@
+
+$(document).ready(function() {
+
 $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
+                || location.hostname == this.hostname) {
+        var hashStr = this.hash.slice(1);
         var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
+        target = target.length ? target : $('[name=' + hashStr +']');
+
+        if (target.length) {
+            $('html,body').animate({ scrollTop: target.offset().top}, 1000);
+            window.location.hash = hashStr;
             return false;
         }
     }
 });
-$(document).ready(function() {
   
   $('#contact-form').bootstrapValidator({
         fields: {
